123bcf51043fa68e5655fa01168e5619218d6ae0
Andreas Drewke <andreas@drewke.net>
2017-07-23 15:03:02 +0200

    Mission #2: use STL data structures / container instead of java ones - replacing some _HashMap with map

--- src/tdme/tools/shared/views/EntityBaseView.cpp
+++ src/tdme/tools/shared/views/EntityBaseView.cpp
@@ -9,7 +9,6 @@
 #include <tdme/tools/shared/model/LevelEditorEntity.h>
 #include <tdme/tools/shared/model/LevelPropertyPresets.h>
 #include <tdme/tools/shared/model/PropertyModelClass.h>
-#include <tdme/utils/_ArrayList.h>
 
 using tdme::tools::shared::views::EntityBaseView;
 using java::lang::Object;
@@ -20,7 +19,6 @@ using tdme::tools::shared::controller::EntityBaseSubScreenController;
 using tdme::tools::shared::model::LevelEditorEntity;
 using tdme::tools::shared::model::LevelPropertyPresets;
 using tdme::tools::shared::model::PropertyModelClass;
-using tdme::utils::_ArrayList;
 
 template<typename T, typename U>
 static T java_cast(U* u)
