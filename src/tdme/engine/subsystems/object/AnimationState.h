c7238f16516e72fbb299188d0b6da69a58e96507
Andreas Drewke <andreas@drewke.net>
2017-08-08 22:14:21 +0200

    Mission #4: unjavaize engine/subsystems/object package

--- src/tdme/engine/subsystems/object/AnimationState.h
+++ src/tdme/engine/subsystems/object/AnimationState.h
@@ -5,46 +5,17 @@
 #include <fwd-tdme.h>
 #include <tdme/engine/model/fwd-tdme.h>
 #include <tdme/engine/subsystems/object/fwd-tdme.h>
-#include <java/lang/Object.h>
 
-using java::lang::Object;
 using tdme::engine::model::AnimationSetup;
-
-
-struct default_init_tag;
-
 /** 
  * Animation state class
  * @author Andreas Drewke
  * @version $Id$
  */
-class tdme::engine::subsystems::object::AnimationState final
-	: public Object
-{
-
-public:
-	typedef Object super;
-
-public: /* protected */
+struct tdme::engine::subsystems::object::AnimationState final {
 	AnimationSetup* setup {  };
 	int64_t currentAtTime {  };
 	int64_t lastAtTime {  };
 	bool finished {  };
 	float time {  };
-
-	// Generated
-
-public:
-	AnimationState();
-protected:
-	void ctor();
-	AnimationState(const ::default_init_tag&);
-
-
-public:
-	static ::java::lang::Class *class_();
-
-private:
-	void init();
-	virtual ::java::lang::Class* getClass0();
 };
