a6b41ca98495a4ddc2bbecf18db2035cff3bfa64
Andreas Drewke <andreas@drewke.net>
2017-08-19 17:24:39 +0200

    Mission #4: unjavaize engine/subsystems/particlesystem package

--- src/tdme/engine/subsystems/particlesystem/PointParticleEmitter.cpp
+++ src/tdme/engine/subsystems/particlesystem/PointParticleEmitter.cpp
@@ -8,7 +8,6 @@
 #include <tdme/engine/subsystems/particlesystem/Particle.h>
 #include <tdme/math/Matrix4x4.h>
 #include <tdme/math/Vector3.h>
-#include <Array.h>
 
 using tdme::engine::subsystems::particlesystem::PointParticleEmitter;
 using java::lang::Math;
@@ -19,33 +18,19 @@ using tdme::engine::subsystems::particlesystem::Particle;
 using tdme::math::Matrix4x4;
 using tdme::math::Vector3;
 
-PointParticleEmitter::PointParticleEmitter(const ::default_init_tag&)
-	: super(*static_cast< ::default_init_tag* >(0))
-{
-	clinit();
-}
-
 PointParticleEmitter::PointParticleEmitter(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, float mass, float massRnd, Vector3* position, Vector3* velocity, Vector3* velocityRnd, Color4* colorStart, Color4* colorEnd) 
-	: PointParticleEmitter(*static_cast< ::default_init_tag* >(0))
 {
-	ctor(count,lifeTime,lifeTimeRnd,mass,massRnd,position,velocity,velocityRnd,colorStart,colorEnd);
-}
-
-void PointParticleEmitter::ctor(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, float mass, float massRnd, Vector3* position, Vector3* velocity, Vector3* velocityRnd, Color4* colorStart, Color4* colorEnd)
-{
-	super::ctor();
 	this->count = count;
 	this->lifeTime = lifeTime;
 	this->lifeTimeRnd = lifeTimeRnd;
 	this->mass = mass;
 	this->massRnd = massRnd;
-	this->position = position;
-	this->positionTransformed = new Vector3();
-	this->positionTransformed->set(position);
-	this->velocity = velocity;
-	this->velocityRnd = velocityRnd;
-	this->colorStart = colorStart;
-	this->colorEnd = colorEnd;
+	this->position.set(position);
+	this->positionTransformed.set(position);
+	this->velocity.set(velocity);
+	this->velocityRnd.set(velocityRnd);
+	this->colorStart.set(colorStart);
+	this->colorEnd.set(colorEnd);
 }
 
 int32_t PointParticleEmitter::getCount()
@@ -55,54 +40,49 @@ int32_t PointParticleEmitter::getCount()
 
 Vector3* PointParticleEmitter::getVelocity()
 {
-	return velocity;
+	return &velocity;
 }
 
 Vector3* PointParticleEmitter::getVelocityRnd()
 {
-	return velocityRnd;
+	return &velocityRnd;
 }
 
 Color4* PointParticleEmitter::getColorStart()
 {
-	return colorStart;
+	return &colorStart;
 }
 
 Color4* PointParticleEmitter::getColorEnd()
 {
-	return colorEnd;
+	return &colorEnd;
 }
 
 void PointParticleEmitter::emit(Particle* particle)
 {
-	auto velocityXYZ = velocity->getArray();
-	auto velocityRndXYZ = velocityRnd->getArray();
+	auto velocityXYZ = velocity.getArray();
+	auto velocityRndXYZ = velocityRnd.getArray();
 	particle->active = true;
-	particle->position->set(positionTransformed);
-	particle->velocity->set((*velocityXYZ)[0] + static_cast< float >((Math::random() * (*velocityRndXYZ)[0] * (Math::random() > 0.5 ? +1.0f : -1.0f))), (*velocityXYZ)[1] + static_cast< float >((Math::random() * (*velocityRndXYZ)[1] * (Math::random() > 0.5 ? +1.0f : -1.0f))), (*velocityXYZ)[2] + static_cast< float >((Math::random() * (*velocityRndXYZ)[2] * (Math::random() > 0.5 ? +1.0f : -1.0f))));
+	particle->position->set(&positionTransformed);
+	particle->velocity->set(
+		(*velocityXYZ)[0] + static_cast< float >((Math::random() * (*velocityRndXYZ)[0] * (Math::random() > 0.5 ? +1.0f : -1.0f))),
+		(*velocityXYZ)[1] + static_cast< float >((Math::random() * (*velocityRndXYZ)[1] * (Math::random() > 0.5 ? +1.0f : -1.0f))),
+		(*velocityXYZ)[2] + static_cast< float >((Math::random() * (*velocityRndXYZ)[2] * (Math::random() > 0.5 ? +1.0f : -1.0f)))
+	);
 	particle->mass = mass + static_cast< float >((Math::random() * (massRnd)));
 	particle->lifeTimeMax = lifeTime + static_cast< int64_t >((Math::random() * lifeTimeRnd));
 	particle->lifeTimeCurrent = 0LL;
-	particle->color->set(static_cast< Color4Base* >(colorStart));
-	particle->colorAdd->set((colorEnd->getRed() - colorStart->getRed()) / particle->lifeTimeMax, (colorEnd->getGreen() - colorStart->getGreen()) / particle->lifeTimeMax, (colorEnd->getBlue() - colorStart->getBlue()) / particle->lifeTimeMax, (colorEnd->getAlpha() - colorStart->getAlpha()) / particle->lifeTimeMax);
+	particle->color->set(&colorStart);
+	particle->colorAdd->set(
+		(colorEnd.getRed() - colorStart.getRed()) / particle->lifeTimeMax,
+		(colorEnd.getGreen() - colorStart.getGreen()) / particle->lifeTimeMax,
+		(colorEnd.getBlue() - colorStart.getBlue()) / particle->lifeTimeMax,
+		(colorEnd.getAlpha() - colorStart.getAlpha()) / particle->lifeTimeMax
+	);
 }
 
 void PointParticleEmitter::fromTransformations(Transformations* transformations)
 {
 	auto transformationsMatrix = transformations->getTransformationsMatrix();
-	transformationsMatrix->multiply(position, positionTransformed);
+	transformationsMatrix->multiply(&position, &positionTransformed);
 }
-
-extern java::lang::Class* class_(const char16_t* c, int n);
-
-java::lang::Class* PointParticleEmitter::class_()
-{
-    static ::java::lang::Class* c = ::class_(u"tdme.engine.subsystems.particlesystem.PointParticleEmitter", 58);
-    return c;
-}
-
-java::lang::Class* PointParticleEmitter::getClass0()
-{
-	return class_();
-}
-
--- src/tdme/engine/subsystems/particlesystem/PointParticleEmitter.h
+++ src/tdme/engine/subsystems/particlesystem/PointParticleEmitter.h
@@ -5,20 +5,17 @@
 #include <fwd-tdme.h>
 #include <tdme/engine/fwd-tdme.h>
 #include <tdme/engine/model/fwd-tdme.h>
+#include <tdme/engine/model/Color4.h>
 #include <tdme/engine/subsystems/particlesystem/fwd-tdme.h>
 #include <tdme/math/fwd-tdme.h>
-#include <java/lang/Object.h>
+#include <tdme/math/Vector3.h>
 #include <tdme/engine/subsystems/particlesystem/ParticleEmitter.h>
 
-using java::lang::Object;
 using tdme::engine::subsystems::particlesystem::ParticleEmitter;
 using tdme::engine::Transformations;
 using tdme::engine::model::Color4;
 using tdme::engine::subsystems::particlesystem::Particle;
 using tdme::math::Vector3;
-
-
-struct default_init_tag;
 
 /** 
  * Point particle emitter
@@ -26,38 +23,21 @@ struct default_init_tag;
  * @version $Id$
  */
 class tdme::engine::subsystems::particlesystem::PointParticleEmitter final
-	: public virtual Object
-	, public ParticleEmitter
+	: public ParticleEmitter
 {
-
-public:
-	typedef Object super;
-
 private:
 	int32_t count {  };
 	int64_t lifeTime {  };
 	int64_t lifeTimeRnd {  };
 	float mass {  };
 	float massRnd {  };
-	Vector3* position {  };
-	Vector3* positionTransformed {  };
-	Vector3* velocity {  };
-	Vector3* velocityRnd {  };
-	Vector3* zeroPosition {  };
-	Color4* colorStart {  };
-	Color4* colorEnd {  };
-protected:
-
-	/** 
-	 * @param number of particles to emit in one second
-	 * @param life time in milli seconds
-	 * @param life time rnd in milli seconds
-	 * @param mass in kg
-	 * @param mass rnd in kg
-	 * @param velocity in meter / seconds
-	 * @param velocity rnd in meter / seconds
-	 */
-	void ctor(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, float mass, float massRnd, Vector3* position, Vector3* velocity, Vector3* velocityRnd, Color4* colorStart, Color4* colorEnd);
+	Vector3 position {  };
+	Vector3 positionTransformed {  };
+	Vector3 velocity {  };
+	Vector3 velocityRnd {  };
+	Vector3 zeroPosition {  };
+	Color4 colorStart {  };
+	Color4 colorEnd {  };
 
 public:
 	int32_t getCount() override;
@@ -68,15 +48,14 @@ public:
 	void emit(Particle* particle) override;
 	void fromTransformations(Transformations* transformations) override;
 
-	// Generated
+	/**
+	 * @param number of particles to emit in one second
+	 * @param life time in milli seconds
+	 * @param life time rnd in milli seconds
+	 * @param mass in kg
+	 * @param mass rnd in kg
+	 * @param velocity in meter / seconds
+	 * @param velocity rnd in meter / seconds
+	 */
 	PointParticleEmitter(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, float mass, float massRnd, Vector3* position, Vector3* velocity, Vector3* velocityRnd, Color4* colorStart, Color4* colorEnd);
-protected:
-	PointParticleEmitter(const ::default_init_tag&);
-
-
-public:
-	static ::java::lang::Class *class_();
-
-private:
-	virtual ::java::lang::Class* getClass0();
 };
