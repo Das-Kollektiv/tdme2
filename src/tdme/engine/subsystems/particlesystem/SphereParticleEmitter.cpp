1aca4f816f1bd5b655bb4d38459c309e4dccbad8
Andreas Drewke <andreas@drewke.net>
2017-08-19 17:29:30 +0200

    Mission #4: unjavaize engine/subsystems/particlesystem package

--- src/tdme/engine/subsystems/particlesystem/SphereParticleEmitter.cpp
+++ src/tdme/engine/subsystems/particlesystem/SphereParticleEmitter.cpp
@@ -8,7 +8,6 @@
 #include <tdme/engine/primitives/Sphere.h>
 #include <tdme/engine/subsystems/particlesystem/Particle.h>
 #include <tdme/math/Vector3.h>
-#include <Array.h>
 
 using tdme::engine::subsystems::particlesystem::SphereParticleEmitter;
 using java::lang::Math;
@@ -19,40 +18,19 @@ using tdme::engine::primitives::Sphere;
 using tdme::engine::subsystems::particlesystem::Particle;
 using tdme::math::Vector3;
 
-template<typename T, typename U>
-static T java_cast(U* u)
-{
-    if (!u) return static_cast<T>(nullptr);
-    auto t = dynamic_cast<T>(u);
-    return t;
-}
-
-SphereParticleEmitter::SphereParticleEmitter(const ::default_init_tag&)
-	: super(*static_cast< ::default_init_tag* >(0))
-{
-	clinit();
-}
-
 SphereParticleEmitter::SphereParticleEmitter(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, float mass, float massRnd, Sphere* sphere, Vector3* velocity, Vector3* velocityRnd, Color4* colorStart, Color4* colorEnd) 
-	: SphereParticleEmitter(*static_cast< ::default_init_tag* >(0))
 {
-	ctor(count,lifeTime,lifeTimeRnd,mass,massRnd,sphere,velocity,velocityRnd,colorStart,colorEnd);
-}
-
-void SphereParticleEmitter::ctor(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, float mass, float massRnd, Sphere* sphere, Vector3* velocity, Vector3* velocityRnd, Color4* colorStart, Color4* colorEnd)
-{
-	super::ctor();
 	this->count = count;
 	this->lifeTime = lifeTime;
 	this->lifeTimeRnd = lifeTimeRnd;
 	this->mass = mass;
 	this->massRnd = massRnd;
 	this->sphere = sphere;
-	this->sphereTransformed = java_cast< Sphere* >(sphere->clone());
-	this->velocity = velocity;
-	this->velocityRnd = velocityRnd;
-	this->colorStart = colorStart;
-	this->colorEnd = colorEnd;
+	this->sphereTransformed = dynamic_cast< Sphere* >(sphere->clone());
+	this->velocity.set(velocity);
+	this->velocityRnd.set(velocityRnd);
+	this->colorStart.set(colorStart);
+	this->colorEnd.set(colorEnd);
 }
 
 int32_t SphereParticleEmitter::getCount()
@@ -62,54 +40,53 @@ int32_t SphereParticleEmitter::getCount()
 
 Vector3* SphereParticleEmitter::getVelocity()
 {
-	return velocity;
+	return &velocity;
 }
 
 Vector3* SphereParticleEmitter::getVelocityRnd()
 {
-	return velocityRnd;
+	return &velocityRnd;
 }
 
 Color4* SphereParticleEmitter::getColorStart()
 {
-	return colorStart;
+	return &colorStart;
 }
 
 Color4* SphereParticleEmitter::getColorEnd()
 {
-	return colorEnd;
+	return &colorEnd;
 }
 
 void SphereParticleEmitter::emit(Particle* particle)
 {
-	auto velocityXYZ = velocity->getArray();
-	auto velocityRndXYZ = velocityRnd->getArray();
+	auto velocityXYZ = velocity.getArray();
+	auto velocityRndXYZ = velocityRnd.getArray();
 	particle->active = true;
-	particle->position->set(static_cast< float >(Math::random()) * 2.0f - 1.0f, static_cast< float >(Math::random()) * 2.0f - 1.0f, static_cast< float >(Math::random()) * 2.0f - 1.0f)->normalize()->scale(sphereTransformed->getRadius());
+	particle->position->set(
+		static_cast< float >(Math::random()) * 2.0f - 1.0f,
+		static_cast< float >(Math::random()) * 2.0f - 1.0f,
+		static_cast< float >(Math::random()) * 2.0f - 1.0f
+	)->normalize()->scale(sphereTransformed->getRadius());
 	particle->position->add(sphereTransformed->getCenter());
-	particle->velocity->set((*velocityXYZ)[0] + static_cast< float >((Math::random() * (*velocityRndXYZ)[0] * (Math::random() > 0.5 ? +1.0f : -1.0f))), (*velocityXYZ)[1] + static_cast< float >((Math::random() * (*velocityRndXYZ)[1] * (Math::random() > 0.5 ? +1.0f : -1.0f))), (*velocityXYZ)[2] + static_cast< float >((Math::random() * (*velocityRndXYZ)[2] * (Math::random() > 0.5 ? +1.0f : -1.0f))));
+	particle->velocity->set(
+		(*velocityXYZ)[0] + static_cast< float >((Math::random() * (*velocityRndXYZ)[0] * (Math::random() > 0.5 ? +1.0f : -1.0f))),
+		(*velocityXYZ)[1] + static_cast< float >((Math::random() * (*velocityRndXYZ)[1] * (Math::random() > 0.5 ? +1.0f : -1.0f))),
+		(*velocityXYZ)[2] + static_cast< float >((Math::random() * (*velocityRndXYZ)[2] * (Math::random() > 0.5 ? +1.0f : -1.0f)))
+	);
 	particle->mass = mass + static_cast< float >((Math::random() * (massRnd)));
 	particle->lifeTimeMax = lifeTime + static_cast< int64_t >((Math::random() * lifeTimeRnd));
 	particle->lifeTimeCurrent = 0LL;
-	particle->color->set(static_cast< Color4Base* >(colorStart));
-	particle->colorAdd->set((colorEnd->getRed() - colorStart->getRed()) / particle->lifeTimeMax, (colorEnd->getGreen() - colorStart->getGreen()) / particle->lifeTimeMax, (colorEnd->getBlue() - colorStart->getBlue()) / particle->lifeTimeMax, (colorEnd->getAlpha() - colorStart->getAlpha()) / particle->lifeTimeMax);
+	particle->color->set(&colorStart);
+	particle->colorAdd->set(
+		(colorEnd.getRed() - colorStart.getRed()) / particle->lifeTimeMax,
+		(colorEnd.getGreen() - colorStart.getGreen()) / particle->lifeTimeMax,
+		(colorEnd.getBlue() - colorStart.getBlue()) / particle->lifeTimeMax,
+		(colorEnd.getAlpha() - colorStart.getAlpha()) / particle->lifeTimeMax
+	);
 }
 
 void SphereParticleEmitter::fromTransformations(Transformations* transformations)
 {
 	sphereTransformed->fromBoundingVolumeWithTransformations(sphere, transformations);
 }
-
-extern java::lang::Class* class_(const char16_t* c, int n);
-
-java::lang::Class* SphereParticleEmitter::class_()
-{
-    static ::java::lang::Class* c = ::class_(u"tdme.engine.subsystems.particlesystem.SphereParticleEmitter", 59);
-    return c;
-}
-
-java::lang::Class* SphereParticleEmitter::getClass0()
-{
-	return class_();
-}
-
--- src/tdme/engine/subsystems/particlesystem/SphereParticleEmitter.h
+++ src/tdme/engine/subsystems/particlesystem/SphereParticleEmitter.h
@@ -5,13 +5,13 @@
 #include <fwd-tdme.h>
 #include <tdme/engine/fwd-tdme.h>
 #include <tdme/engine/model/fwd-tdme.h>
+#include <tdme/engine/model/Color4.h>
 #include <tdme/engine/primitives/fwd-tdme.h>
 #include <tdme/engine/subsystems/particlesystem/fwd-tdme.h>
 #include <tdme/math/fwd-tdme.h>
-#include <java/lang/Object.h>
+#include <tdme/math/Vector3.h>
 #include <tdme/engine/subsystems/particlesystem/ParticleEmitter.h>
 
-using java::lang::Object;
 using tdme::engine::subsystems::particlesystem::ParticleEmitter;
 using tdme::engine::Transformations;
 using tdme::engine::model::Color4;
@@ -19,22 +19,14 @@ using tdme::engine::primitives::Sphere;
 using tdme::engine::subsystems::particlesystem::Particle;
 using tdme::math::Vector3;
 
-
-struct default_init_tag;
-
 /** 
  * Sphere particle emitter
  * @author Andreas Drewke
  * @version $Id$
  */
 class tdme::engine::subsystems::particlesystem::SphereParticleEmitter final
-	: public virtual Object
-	, public ParticleEmitter
+	: public ParticleEmitter
 {
-
-public:
-	typedef Object super;
-
 private:
 	int32_t count {  };
 	int64_t lifeTime {  };
@@ -43,13 +35,21 @@ private:
 	float massRnd {  };
 	Sphere* sphere {  };
 	Sphere* sphereTransformed {  };
-	Vector3* velocity {  };
-	Vector3* velocityRnd {  };
-	Color4* colorStart {  };
-	Color4* colorEnd {  };
-protected:
+	Vector3 velocity {  };
+	Vector3 velocityRnd {  };
+	Color4 colorStart {  };
+	Color4 colorEnd {  };
 
-	/** 
+public:
+	int32_t getCount() override;
+	Vector3* getVelocity() override;
+	Vector3* getVelocityRnd() override;
+	Color4* getColorStart() override;
+	Color4* getColorEnd() override;
+	void emit(Particle* particle) override;
+	void fromTransformations(Transformations* transformations) override;
+
+	/**
 	 * @param number of particles to emit in one second
 	 * @param life time in milli seconds
 	 * @param life time rnd in milli seconds
@@ -61,26 +61,5 @@ protected:
 	 * @param color start
 	 * @param color end
 	 */
-	void ctor(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, float mass, float massRnd, Sphere* sphere, Vector3* velocity, Vector3* velocityRnd, Color4* colorStart, Color4* colorEnd);
-
-public:
-	int32_t getCount() override;
-	Vector3* getVelocity() override;
-	Vector3* getVelocityRnd() override;
-	Color4* getColorStart() override;
-	Color4* getColorEnd() override;
-	void emit(Particle* particle) override;
-	void fromTransformations(Transformations* transformations) override;
-
-	// Generated
 	SphereParticleEmitter(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, float mass, float massRnd, Sphere* sphere, Vector3* velocity, Vector3* velocityRnd, Color4* colorStart, Color4* colorEnd);
-protected:
-	SphereParticleEmitter(const ::default_init_tag&);
-
-
-public:
-	static ::java::lang::Class *class_();
-
-private:
-	virtual ::java::lang::Class* getClass0();
 };
