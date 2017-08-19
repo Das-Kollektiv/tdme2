e4bc352d9975f73c3541559ebb97cf18c0b22e70
Andreas Drewke <andreas@drewke.net>
2017-08-19 17:07:02 +0200

    Mission #4: unjavaize engine/subsystems/particlesystem package

--- src/tdme/engine/subsystems/particlesystem/CircleParticleEmitterPlaneVelocity.cpp
+++ src/tdme/engine/subsystems/particlesystem/CircleParticleEmitterPlaneVelocity.cpp
@@ -18,41 +18,25 @@ using tdme::engine::subsystems::particlesystem::Particle;
 using tdme::math::Matrix4x4;
 using tdme::math::Vector3;
 
-CircleParticleEmitterPlaneVelocity::CircleParticleEmitterPlaneVelocity(const ::default_init_tag&)
-	: super(*static_cast< ::default_init_tag* >(0))
-{
-	clinit();
-}
-
 CircleParticleEmitterPlaneVelocity::CircleParticleEmitterPlaneVelocity(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, Vector3* axis0, Vector3* axis1, Vector3* center, float radius, float mass, float massRnd, float velocity, float velocityRnd, Color4* colorStart, Color4* colorEnd) 
-	: CircleParticleEmitterPlaneVelocity(*static_cast< ::default_init_tag* >(0))
 {
-	ctor(count,lifeTime,lifeTimeRnd,axis0,axis1,center,radius,mass,massRnd,velocity,velocityRnd,colorStart,colorEnd);
-}
-
-void CircleParticleEmitterPlaneVelocity::ctor(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, Vector3* axis0, Vector3* axis1, Vector3* center, float radius, float mass, float massRnd, float velocity, float velocityRnd, Color4* colorStart, Color4* colorEnd)
-{
-	super::ctor();
 	this->count = count;
 	this->lifeTime = lifeTime;
 	this->lifeTimeRnd = lifeTimeRnd;
-	this->axis0 = axis0->normalize();
-	this->axis1 = axis1->normalize();
-	this->center = center;
+	this->axis0.set(axis0)->normalize();
+	this->axis1.set(axis1)->normalize();
+	this->center.set(center);
 	this->radius = radius;
 	this->mass = mass;
 	this->massRnd = massRnd;
 	this->velocity = velocity;
 	this->velocityRnd = velocityRnd;
-	this->colorStart = colorStart;
-	this->colorEnd = colorEnd;
-	this->centerTransformed = this->center->clone();
+	this->colorStart.set(colorStart);
+	this->colorEnd.set(colorEnd);
+	this->centerTransformed.set(center);
 	this->radiusTransformed = radius;
-	this->axis0Transformed = axis0->clone();
-	this->axis1Transformed = axis1->clone();
-	this->side = new Vector3();
-	this->cosOnAxis0 = new Vector3();
-	this->sinOnAxis1 = new Vector3();
+	this->axis0Transformed.set(axis0)->normalize();
+	this->axis1Transformed.set(axis1)->normalize();
 }
 
 int32_t CircleParticleEmitterPlaneVelocity::getCount()
@@ -72,53 +56,47 @@ Vector3* CircleParticleEmitterPlaneVelocity::getVelocityRnd()
 
 Color4* CircleParticleEmitterPlaneVelocity::getColorStart()
 {
-	return colorStart;
+	return &colorStart;
 }
 
 Color4* CircleParticleEmitterPlaneVelocity::getColorEnd()
 {
-	return colorEnd;
+	return &colorEnd;
 }
 
 void CircleParticleEmitterPlaneVelocity::emit(Particle* particle)
 {
+	Vector3 cosOnAxis0;
+	Vector3 sinOnAxis1;
+	Vector3 side;
 	particle->active = true;
 	auto rnd = static_cast< float >(Math::random());
-	cosOnAxis0->set(axis0Transformed)->scale(static_cast< float >(Math::cos(Math::PI * 2 * rnd)));
-	sinOnAxis1->set(axis1Transformed)->scale(static_cast< float >(Math::sin(Math::PI * 2 * rnd)));
-	particle->position->set(cosOnAxis0);
-	particle->position->add(sinOnAxis1);
+	cosOnAxis0.set(&axis0Transformed)->scale(static_cast< float >(Math::cos(Math::PI * 2 * rnd)));
+	sinOnAxis1.set(&axis1Transformed)->scale(static_cast< float >(Math::sin(Math::PI * 2 * rnd)));
+	particle->position->set(&cosOnAxis0);
+	particle->position->add(&sinOnAxis1);
 	particle->position->scale(radiusTransformed);
-	particle->position->add(centerTransformed);
-	particle->velocity->set(particle->position)->sub(centerTransformed)->normalize()->scale(velocity + static_cast< float >((Math::random() * velocityRnd)));
+	particle->position->add(&centerTransformed);
+	particle->velocity->set(particle->position)->sub(&centerTransformed)->normalize()->scale(velocity + static_cast< float >((Math::random() * velocityRnd)));
 	particle->mass = mass + static_cast< float >((Math::random() * (massRnd)));
 	particle->lifeTimeMax = lifeTime + static_cast< int64_t >((Math::random() * lifeTimeRnd));
 	particle->lifeTimeCurrent = 0LL;
-	particle->color->set(static_cast< Color4Base* >(colorStart));
-	particle->colorAdd->set((colorEnd->getRed() - colorStart->getRed()) / particle->lifeTimeMax, (colorEnd->getGreen() - colorStart->getGreen()) / particle->lifeTimeMax, (colorEnd->getBlue() - colorStart->getBlue()) / particle->lifeTimeMax, (colorEnd->getAlpha() - colorStart->getAlpha()) / particle->lifeTimeMax);
+	particle->color->set(&colorStart);
+	particle->colorAdd->set(
+		(colorEnd.getRed() - colorStart.getRed()) / particle->lifeTimeMax,
+		(colorEnd.getGreen() - colorStart.getGreen()) / particle->lifeTimeMax,
+		(colorEnd.getBlue() - colorStart.getBlue()) / particle->lifeTimeMax,
+		(colorEnd.getAlpha() - colorStart.getAlpha()) / particle->lifeTimeMax);
 }
 
 void CircleParticleEmitterPlaneVelocity::fromTransformations(Transformations* transformations)
 {
+	Vector3 side;
 	auto transformationsMatrix = transformations->getTransformationsMatrix();
-	transformationsMatrix->multiply(center, centerTransformed);
-	transformationsMatrix->multiplyNoTranslation(axis0, axis0Transformed);
-	transformationsMatrix->multiplyNoTranslation(axis1, axis1Transformed);
-	side->set(axis0)->scale(radius)->add(center);
-	transformationsMatrix->multiply(side, side);
-	radius = side->sub(center)->computeLength();
+	transformationsMatrix->multiply(&center, &centerTransformed);
+	transformationsMatrix->multiplyNoTranslation(&axis0, &axis0Transformed);
+	transformationsMatrix->multiplyNoTranslation(&axis1, &axis1Transformed);
+	side.set(&axis0)->scale(radius)->add(&center);
+	transformationsMatrix->multiply(&side, &side);
+	radius = side.sub(&center)->computeLength();
 }
-
-extern java::lang::Class* class_(const char16_t* c, int n);
-
-java::lang::Class* CircleParticleEmitterPlaneVelocity::class_()
-{
-    static ::java::lang::Class* c = ::class_(u"tdme.engine.subsystems.particlesystem.CircleParticleEmitterPlaneVelocity", 72);
-    return c;
-}
-
-java::lang::Class* CircleParticleEmitterPlaneVelocity::getClass0()
-{
-	return class_();
-}
-
--- src/tdme/engine/subsystems/particlesystem/CircleParticleEmitterPlaneVelocity.h
+++ src/tdme/engine/subsystems/particlesystem/CircleParticleEmitterPlaneVelocity.h
@@ -5,20 +5,17 @@
 #include <fwd-tdme.h>
 #include <tdme/engine/fwd-tdme.h>
 #include <tdme/engine/model/fwd-tdme.h>
+#include <tdme/engine/model/Color4.h>
 #include <tdme/engine/subsystems/particlesystem/fwd-tdme.h>
 #include <tdme/math/fwd-tdme.h>
-#include <java/lang/Object.h>
+#include <tdme/math/Vector3.h>
 #include <tdme/engine/subsystems/particlesystem/ParticleEmitter.h>
 
-using java::lang::Object;
 using tdme::engine::subsystems::particlesystem::ParticleEmitter;
 using tdme::engine::Transformations;
 using tdme::engine::model::Color4;
 using tdme::engine::subsystems::particlesystem::Particle;
 using tdme::math::Vector3;
-
-
-struct default_init_tag;
 
 /** 
  * Circle particle emitter with velocity that lives in plane only 
@@ -26,35 +23,35 @@ struct default_init_tag;
  * @version $Id$
  */
 class tdme::engine::subsystems::particlesystem::CircleParticleEmitterPlaneVelocity final
-	: public virtual Object
-	, public ParticleEmitter
+	: public ParticleEmitter
 {
-
-public:
-	typedef Object super;
-
 private:
 	int32_t count {  };
 	int64_t lifeTime {  };
 	int64_t lifeTimeRnd {  };
-	Vector3* axis0 {  };
-	Vector3* axis1 {  };
-	Vector3* center {  };
+	Vector3 axis0 {  };
+	Vector3 axis1 {  };
+	Vector3 center {  };
 	float radius {  };
 	float mass {  };
 	float massRnd {  };
 	float velocity {  };
 	float velocityRnd {  };
-	Color4* colorStart {  };
-	Color4* colorEnd {  };
-	Vector3* centerTransformed {  };
+	Color4 colorStart {  };
+	Color4 colorEnd {  };
+	Vector3 centerTransformed {  };
 	float radiusTransformed {  };
-	Vector3* axis0Transformed {  };
-	Vector3* axis1Transformed {  };
-	Vector3* side {  };
-	Vector3* cosOnAxis0 {  };
-	Vector3* sinOnAxis1 {  };
-protected:
+	Vector3 axis0Transformed {  };
+	Vector3 axis1Transformed {  };
+
+public:
+	int32_t getCount() override;
+	Vector3* getVelocity() override;
+	Vector3* getVelocityRnd() override;
+	Color4* getColorStart() override;
+	Color4* getColorEnd() override;
+	void emit(Particle* particle) override;
+	void fromTransformations(Transformations* transformations) override;
 
 	/** 
 	 * Public constructor
@@ -72,26 +69,5 @@ protected:
 	 * @param color start
 	 * @param color end
 	 */
-	void ctor(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, Vector3* axis0, Vector3* axis1, Vector3* center, float radius, float mass, float massRnd, float velocity, float velocityRnd, Color4* colorStart, Color4* colorEnd);
-
-public:
-	int32_t getCount() override;
-	Vector3* getVelocity() override;
-	Vector3* getVelocityRnd() override;
-	Color4* getColorStart() override;
-	Color4* getColorEnd() override;
-	void emit(Particle* particle) override;
-	void fromTransformations(Transformations* transformations) override;
-
-	// Generated
 	CircleParticleEmitterPlaneVelocity(int32_t count, int64_t lifeTime, int64_t lifeTimeRnd, Vector3* axis0, Vector3* axis1, Vector3* center, float radius, float mass, float massRnd, float velocity, float velocityRnd, Color4* colorStart, Color4* colorEnd);
-protected:
-	CircleParticleEmitterPlaneVelocity(const ::default_init_tag&);
-
-
-public:
-	static ::java::lang::Class *class_();
-
-private:
-	virtual ::java::lang::Class* getClass0();
 };
