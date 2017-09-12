36eff2a31634f9662b80dbf08e98e070a0e10c4c
Andreas Drewke <andreas@drewke.net>
2017-09-12 13:32:46 +0200

    Mission #7: optimizations by inlining

--- src/tdme/math/Vector2.h
+++ src/tdme/math/Vector2.h
@@ -4,11 +4,13 @@
 
 #include <array>
 
-#include <Array.h>
 #include <fwd-tdme.h>
 #include <tdme/math/fwd-tdme.h>
+#include <tdme/math/Math.h>
 
 using std::array;
+
+using tdme::math::Math;
 
 /** 
  * Vector 2 class
@@ -28,103 +30,150 @@ public:
 	 * @param y
 	 * @return this vector
 	 */
-	Vector2* set(float x, float y);
+	inline Vector2* set(float x, float y) {
+		data[0] = x;
+		data[1] = y;
+		return this;
+	}
 
 	/** 
 	 * Set up vector
 	 * @param float array containing x,y values
 	 * @return this vector
 	 */
-	Vector2* set(array<float, 2>* v);
+	inline Vector2* set(array<float, 2>* v) {
+		data = *v;
+		return this;
+	}
 
 	/** 
 	 * Set up vector
 	 * @param v
 	 * @return this vector
 	 */
-	Vector2* set(Vector2* v);
+	inline Vector2* set(Vector2* v) {
+		data = v->data;
+		return this;
+	}
 
 	/** 
 	 * @return x
 	 */
-	float getX();
+	inline float getX() {
+		return data[0];
+	}
 
 	/** 
 	 * set X
 	 * @param x
 	 */
-	void setX(float x);
+	inline void setX(float x) {
+		data[0] = x;
+	}
 
 	/** 
 	 * @return y
 	 */
-	float getY();
+	inline float getY() {
+		return data[1];
+	}
 
 	/** 
 	 * set Y
 	 * @param y
 	 */
-	void setY(float y);
+	inline void setY(float y) {
+		data[1] = y;
+	}
 
 	/** 
 	 * Adds a vector
 	 * @param v
 	 * @return this vector
 	 */
-	Vector2* add(Vector2* v);
+	inline Vector2* add(Vector2* v) {
+		data[0] += v->data[0];
+		data[1] += v->data[1];
+		return this;
+	}
 
 	/** 
 	 * Subtracts a vector
 	 * @param v
 	 * @return this vector
 	 */
-	Vector2* sub(Vector2* v);
+	inline Vector2* sub(Vector2* v) {
+		data[0] -= v->data[0];
+		data[1] -= v->data[1];
+		return this;
+	}
 
 	/** 
 	 * Scale this vector
 	 * @param scale
 	 * @return this vector 
 	 */
-	Vector2* scale(float scale);
+	inline Vector2* scale(float scale) {
+		data[0] *= scale;
+		data[1] *= scale;
+		return this;
+	}
 
 	/** 
 	 * Scale this vector
 	 * @param scale
 	 * @return this vector 
 	 */
-	Vector2* scale(Vector2* scale);
+	inline Vector2* scale(Vector2* scale) {
+		data[0] *= scale->data[0];
+		data[1] *= scale->data[1];
+		return this;
+	}
 
 	/** 
 	 * @return the vectors length
 	 */
-	float computeLength();
+	inline float computeLength() {
+		return Math::sqrt((data[0] * data[0]) + (data[1] * data[1]));
+	}
 
 	/** 
 	 * @return vector as array
 	 */
-	array<float, 2>* getArray();
+	inline array<float, 2>* getArray() {
+		return &data;
+	}
 
 	/**
 	 * Public constructor
 	 */
-	Vector2();
+	inline Vector2() {
+		data.fill(0.0f);
+	}
 
 	/**
 	 * Public constructor
 	 * @param x
 	 * @param y
 	 */
-	Vector2(float x, float y);
+	inline Vector2(float x, float y) {
+		data[0] = x;
+		data[1] = y;
+	}
 
 	/**
 	 * Public constructor
 	 * @param float values
 	 */
-	Vector2(array<float, 2>* v);
+	inline Vector2(array<float, 2>* v) {
+		data = *v;
+	}
 
 	/**
 	 * Public constructor
 	 * @param v
 	 */
-	Vector2(Vector2* v);
+	inline Vector2(Vector2* v) {
+		data = v->data;
+	}
 };
