98016f919028f4cab5d6e65ad978dcd916dd347c
Andreas Drewke <andreas@drewke.net>
2018-01-04 01:05:11 +0100

    Mission #12: import code comments from tdme(-java) #engine/subsystems/object #WIP

--- src/tdme/engine/subsystems/object/Object3DGroupVBORenderer.cpp
+++ src/tdme/engine/subsystems/object/Object3DGroupVBORenderer.cpp
@@ -36,30 +36,37 @@ Object3DGroupVBORenderer::Object3DGroupVBORenderer(Object3DGroup* object3DGroup)
 	this->object3DGroup = object3DGroup;
 	this->vboBaseIds = nullptr;
 	this->vboTangentBitangentIds = nullptr;
-	this->vboSkinningIds = nullptr;
 }
 
 void Object3DGroupVBORenderer::preRender(Object3DVBORenderer* object3DVBORenderer)
 {
 	auto meshUploaded = true;
+	// initialize if not yet done
 	if (vboBaseIds == nullptr) {
 		auto vboManaged = Engine::getInstance()->getVBOManager()->addVBO(object3DGroup->id, object3DGroup->mesh->group->getTextureCoordinates() != nullptr ? 4 : 3);
 		vboBaseIds = vboManaged->getVBOGlIds();
 		meshUploaded = vboManaged->isUploaded();
 	}
+	// initialize tangents, bitangents
 	if (object3DVBORenderer->renderer->isNormalMappingAvailable() && object3DGroup->mesh->group->getTangents()->size() > 0 && object3DGroup->mesh->group->getBitangents()->size() > 0 && vboTangentBitangentIds == nullptr) {
 		auto vboManaged = Engine::getInstance()->getVBOManager()->addVBO(object3DGroup->id + ".tangentbitangent", 2);
 		vboTangentBitangentIds = vboManaged->getVBOGlIds();
 	}
+	// check if to upload new mesh
 	if (object3DGroup->mesh->hasRecreatedBuffers() == true || meshUploaded == false) {
 		if (meshUploaded == false) {
+			// upload indices
 			object3DGroup->mesh->setupVertexIndicesBuffer(object3DVBORenderer->renderer, (*vboBaseIds)[0]);
+			// upload texture coordinates
 			if (object3DGroup->mesh->group->getTextureCoordinates() != nullptr) {
 				object3DGroup->mesh->setupTextureCoordinatesBuffer(object3DVBORenderer->renderer, (*vboBaseIds)[3]);
 			}
 		}
+		// upload vertices
 		object3DGroup->mesh->setupVerticesBuffer(object3DVBORenderer->renderer, (*vboBaseIds)[1]);
+		// upload normals
 		object3DGroup->mesh->setupNormalsBuffer(object3DVBORenderer->renderer, (*vboBaseIds)[2]);
+		// tangents, bitangents
 		if (vboTangentBitangentIds != nullptr) {
 			object3DGroup->mesh->setupTangentsBuffer(object3DVBORenderer->renderer, (*vboTangentBitangentIds)[0]);
 			object3DGroup->mesh->setupBitangentsBuffer(object3DVBORenderer->renderer, (*vboTangentBitangentIds)[1]);
@@ -74,7 +81,7 @@ void Object3DGroupVBORenderer::dispose()
 		vboBaseIds = nullptr;
 	}
 	if (vboSkinningIds != nullptr) {
-		Engine::getInstance()->getVBOManager()->removeVBO(object3DGroup->id + ".skinning");
+		Engine::getInstance()->getVBOManager()->removeVBO(object3DGroup->id + ".tangentbitangent");
 		vboSkinningIds = nullptr;
 	}
 }
