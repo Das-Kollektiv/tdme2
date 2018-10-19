fbe51812d9542f543a220e9bfcd5de5c19977bd8
Andreas Drewke <andreas@drewke.net>
2018-10-19 16:49:27 +0200

    utils/Float: Have a interpolateLinear method

--- src/tdme/utils/Float.h
+++ src/tdme/utils/Float.h
@@ -44,4 +44,16 @@ public:
 		return isnan(value);
 	}
 
+	/**
+	 * Interpolates between float 1 and float 2 by 0f<=t<=1f linearly
+	 * @param f1 float 1
+	 * @param f2 float 2
+	 * @param t t
+	 * @return interpolated float value
+	 */
+	inline static float interpolateLinear(float f1, float f2, float t) {
+		return (f2 * t) + ((1.0f - t) * f1);
+	}
+
+
 };
