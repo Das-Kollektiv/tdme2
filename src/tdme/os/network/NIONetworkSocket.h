7cc814af9732d0746ddf49b154982e0f91a028ac
Andreas Drewke <andreas@drewke.net>
2019-04-15 16:21:31 +0200

    port-mingw: mingw is trolling me

--- src/tdme/os/network/NIONetworkSocket.h
+++ src/tdme/os/network/NIONetworkSocket.h
@@ -88,11 +88,11 @@ protected:
 // MINGW: Have some missing posix functions
 #if defined(__MINGW32__)
 	// TODO: move those somewhere else as they are not only socket specific
+	#define inet_pton inet_pton6
+	#define inet_ntop inet_ntop6
 	size_t strlcpy(char* __restrict dst, const char* __restrict src, size_t siz);
 	int inet_pton4(const char* src, void* dst);
 	int inet_pton6(int af, const char* src, void* dst);
 	char* inet_ntop4(const void* src, char* dst, size_t size);
 	char* inet_ntop6(int af, const void* src, char* dst, size_t size);
-	#define inet_pton inet_pton6
-	#define inet_ntop inet_ntop6
 #endif
