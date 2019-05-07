95950b4fea7145cc3c13874ecbc716cdfc953d02
U-WINDOWS-DESKTOP\Andreas <andreas@drewke.net>
2019-05-07 03:35:41 +0200

    renderer-vulkan: this fixes some vulkan validation messages

--- src/tdme/engine/subsystems/renderer/VKRenderer.cpp
+++ src/tdme/engine/subsystems/renderer/VKRenderer.cpp
@@ -1151,7 +1151,7 @@ void VKRenderer::initializeRenderPass() {
 			.storeOp = VK_ATTACHMENT_STORE_OP_STORE,
 			.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
 			.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
-			.initialLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+			.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
 			.finalLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL
 		},
 		[1] = {
