dfa8dd4b37e628cdc9b3b189897e549bfc1b7be7
Andreas Drewke <andreas@drewke.net>
2019-07-15 19:33:13 +0200

    tests: Have a raytracing test as we have issues with ASW, the error is reproducable, need to check if the fault is on my side

---
+++ src/tdme/tests/RayTracingTest.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <tdme/tdme.h>
+#include <tdme/application/InputEventHandler.h>
+#include <tdme/engine/fwd-tdme.h>
+#include <tdme/engine/physics/fwd-tdme.h>
+#include <tdme/application/Application.h>
+#include <tdme/tests/fwd-tdme.h>
+#include <tdme/application/InputEventHandler.h>
+
+using tdme::application::Application;
+using tdme::engine::Engine;
+using tdme::engine::physics::World;
+
+/** 
+ * Ray Tracing Test
+ * @author andreas.drewke
+ * @version $Id$
+ */
+class tdme::tests::RayTracingTest final
+	: public virtual Application, public virtual InputEventHandler
+{
+private:
+	static constexpr int32_t RIGID_TYPEID_STANDARD { 1 };
+	static constexpr int32_t BOX_COUNT { 4 };
+	Engine* engine {  };
+	World* world {  };
+
+	bool keyLeft { false };
+	bool keyRight { false };
+	bool keyUp { false };
+	bool keyDown { false };
+	int mouseLastXDragged { -1 };
+	int mouseLastYDragged { -1 };
+
+	float rotationY { 0.0f };
+	float rotationX { 0.0f };
+
+public:
+
+	/** 
+	 * Main
+	 * @param argc argument count
+	 * @param argv argument values
+	 */
+	static void main(int argc, char** argv);
+public:
+	void display() override;
+	void dispose() override;
+	void initialize() override;
+	void reshape(int32_t width, int32_t height) override;
+
+	// override methods
+	void onKeyDown (unsigned char key, int x, int y) override;
+	void onKeyUp(unsigned char key, int x, int y) override;
+	void onSpecialKeyDown (int key, int x, int y) override;
+	void onSpecialKeyUp(int key, int x, int y) override;
+	void onMouseDragged(int x, int y) override;
+	void onMouseMoved(int x, int y) override;
+	void onMouseButton(int button, int state, int x, int y) override;
+	void onMouseWheel(int button, int direction, int x, int y) override;
+
+	/**
+	 * Public constructor
+	 */
+	RayTracingTest();
+};
