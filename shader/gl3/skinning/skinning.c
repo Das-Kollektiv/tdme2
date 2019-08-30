5bdede52e7be18e4cceb2ea11460093a8cf4db95
Andreas Drewke <andreas@drewke.net>
2019-08-30 19:52:42 +0200

    Shader: Skinning: Fixed skinning shader for ATI and NVIDIA, lol

--- shader/gl3/skinning/skinning.c
+++ shader/gl3/skinning/skinning.c
@@ -5,8 +5,8 @@ layout(local_size_x=16) in;
 layout(std430,binding=0) buffer InVertex { float inVertexFloatArray[]; };
 layout(std430,binding=1) buffer InNormal { float inNormalFloatArray[]; };
 layout(std430,binding=2) buffer InSkinningVertexJoints { int inSkinningVertexJoints[]; };
-layout(std430,binding=3) buffer InSkinningVertexJointIdxs { int inSkinningVertexJointIdxs[][4]; };
-layout(std430,binding=4) buffer InSkinningVertexJointWeights { float inSkinningVertexJointWeights[][4]; };
+layout(std430,binding=3) buffer InSkinningVertexJointIdxs { int inSkinningVertexJointIdxs[]; };
+layout(std430,binding=4) buffer InSkinningVertexJointWeights { float inSkinningVertexJointWeights[]; };
 layout(std430,binding=5) buffer OutVertex { float outVertexFloatArray[]; };
 layout(std430,binding=6) buffer OutNormal { float outNormalFloatArray[]; };
 layout(std430,binding=7) buffer InSkinningJointsTransformationMatrices{ mat4 inSkinningJointsTransformationsMatrices[]; };
@@ -24,10 +24,10 @@ void main() {
 		vec3 inVertex = vec3(inVertexFloatArray[offset * 3 + 0], inVertexFloatArray[offset * 3 + 1], inVertexFloatArray[offset * 3 + 2]);
 		vec3 inNormal = vec3(inNormalFloatArray[offset * 3 + 0], inNormalFloatArray[offset * 3 + 1], inNormalFloatArray[offset * 3 + 2]);
 		for (int i = 0; i < inSkinningVertexJoints[offset]; i++) {
-			mat4 transformationsMatrix = inSkinningJointsTransformationsMatrices[inSkinningVertexJointIdxs[offset][i]];
-			skinnedVertex+= (transformationsMatrix * vec4(inVertex, 1.0)).xyz * inSkinningVertexJointWeights[offset][i];
-			skinnedNormal+= (mat3(transformationsMatrix) * inNormal) * inSkinningVertexJointWeights[offset][i];
-			totalWeights+= inSkinningVertexJointWeights[offset][i];
+			mat4 transformationsMatrix = inSkinningJointsTransformationsMatrices[inSkinningVertexJointIdxs[offset * 4 + i]];
+			skinnedVertex+= (transformationsMatrix * vec4(inVertex, 1.0)).xyz * inSkinningVertexJointWeights[offset * 4 + i];
+			skinnedNormal+= (mat3(transformationsMatrix) * inNormal) * inSkinningVertexJointWeights[offset * 4 + i];
+			totalWeights+= inSkinningVertexJointWeights[offset * 4 + i];
 		}
 		if (totalWeights != 1.0) {
 			float weightNormalized = totalWeights != 0.0?1.0 / totalWeights:0.0;
