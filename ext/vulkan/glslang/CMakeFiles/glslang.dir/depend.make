67739ec1c9b0aa0e03fb6b8085a9ff4da1ff22c9
Andreas Drewke <andreas@drewke.net>
2019-10-06 10:58:14 +0200

    ext: some clean up

--- ext/vulkan/glslang/CMakeFiles/glslang.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for glslang.
-# This may be replaced when dependencies are built.
--- ext/vulkan/glslang/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/home/andreas/Downloads/1.1.101.0/source/glslang/glslang
-# Build directory: /usr/home/andreas/Downloads/1.1.101.0/source/glslang/glslang
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("OSDependent/Unix")
