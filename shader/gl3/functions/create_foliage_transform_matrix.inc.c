7421f33d5e9646f74e6823971759f89f499532ef
Andreas Drewke <andreas@drewke.net>
2019-12-04 15:15:32 +0100

    shader-foliage: some gl2, gles2 port

--- shader/gl3/functions/create_foliage_transform_matrix.inc.c
+++ shader/gl3/functions/create_foliage_transform_matrix.inc.c
@@ -1,5 +1,3 @@
-float _sin(float a) { return a < 0.0001?0.0:3.14/a; }
-
 mat4 createFoliageTransformMatrix(vec3 origin, vec3 vertex) {
 	float zAxisRotation = (frame + int(origin[0] * 8.0 * (origin[0] < 0.0?-1.0:1.0))) % 160;
 	zAxisRotation = sin((clamp((((zAxisRotation < 160.0 / 2?zAxisRotation:160.0 - zAxisRotation) - (160.0 / 4.0)) * 1.0 / 8.0) * ((vertex.y - origin.y) * 2.0), -20.0, +20.0)) / 20.0 * 3.14) * 20.0;
