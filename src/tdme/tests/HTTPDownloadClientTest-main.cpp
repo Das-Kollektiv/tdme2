01f872f952b7cc212873df4617f61073d2103698
Andreas Drewke <andreas@drewke.net>
2019-12-25 00:05:15 +0100

    Ooooops, forgot about this one

---
+++ src/tdme/tests/HTTPDownloadClientTest-main.cpp
@@ -0,0 +1,33 @@
+#include <tdme/os/network/Network.h>
+#include <tdme/utils/Console.h>
+#include <tdme/utils/Exception.h>
+#include <tdme/network/httpclient/HTTPDownloadClient.h>
+#include <tdme/os/threading/Thread.h>
+
+using std::string;
+using std::stringstream;
+using std::to_string;
+using std::unordered_map;
+using std::vector;
+
+using tdme::network::httpclient::HTTPDownloadClient;
+using tdme::os::network::Network;
+using tdme::os::threading::Thread;
+using tdme::utils::Console;
+using tdme::utils::Exception;
+
+int main(int argc, char *argv[]) {
+	Network::initialize();
+
+	try {
+		HTTPDownloadClient httpDownloadClient;
+		httpDownloadClient.setFile("tdme2-freebsd-x64_2018-07-07-03-29.tgz");
+		httpDownloadClient.setURL("http://drewke.net/tdme2/tdme2-freebsd-x64_2018-07-07-03-29.tgz");
+		httpDownloadClient.execute();
+		while (httpDownloadClient.isFinished() == false) Thread::sleep(100LL);
+		httpDownloadClient.join();
+	} catch (Exception& exception) {
+		Console::println(string("Fail: ") + exception.what());
+	}
+}
+
