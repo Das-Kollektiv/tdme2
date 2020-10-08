e4c0f4b5068f42021f2f95588a9addeddcf1c787
Andreas Drewke <andreas@drewke.net>
2020-10-08 14:46:53 +0200

    tools/cli/converttotm: updated to tool to be working with * file name expansion / batch exporting

--- src/tdme/tools/cli/converttotm-main.cpp
+++ src/tdme/tools/cli/converttotm-main.cpp
@@ -8,6 +8,7 @@
 #include <tdme/os/filesystem/FileSystemInterface.h>
 #include <tdme/utilities/Console.h>
 #include <tdme/utilities/Exception.h>
+#include <tdme/utilities/StringTools.h>
 
 using tdme::application::Application;
 using tdme::engine::fileio::models::ModelReader;
@@ -16,31 +17,34 @@ using tdme::os::filesystem::FileSystem;
 using tdme::os::filesystem::FileSystemInterface;
 using tdme::utilities::Console;
 using tdme::utilities::Exception;
+using tdme::utilities::StringTools;
 
 int main(int argc, char** argv)
 {
 	Console::println(string("converttotm 1.9.9"));
 	Console::println(string("Programmed 2018 by Andreas Drewke, drewke.net."));
 	Console::println();
-	if (argc != 3) {
-		Console::println("Usage: converttotm inputfile outputfile");
+	if (argc < 2) {
+		Console::println("Usage: converttotm inputfile1 [inputfileN]");
 		Application::exit(1);
 	}
-	string inputFileName = string(argv[1]);
-	string outputFileName = string(argv[2]);
-	try {
-		Console::println("Loading model: " + inputFileName);
-		auto model = ModelReader::read(
-			FileSystem::getInstance()->getPathName(inputFileName),
-			FileSystem::getInstance()->getFileName(inputFileName)
-		);
-		Console::println("Exporting model: " + outputFileName);
-		TMWriter::write(
-			model,
-			FileSystem::getInstance()->getPathName(outputFileName),
-			FileSystem::getInstance()->getFileName(outputFileName)
-		);
-	} catch (Exception& exception) {
-		Console::println("An error occurred: " + string(exception.what()));
+	for (auto i = 1; i < argc; i++) {
+		string inputFileName = argv[i];
+		string outputFileName = StringTools::substring(inputFileName, 0, inputFileName.rfind('.')) + ".tm";
+		try {
+			Console::println("Loading model: " + inputFileName);
+			auto model = ModelReader::read(
+				FileSystem::getInstance()->getPathName(inputFileName),
+				FileSystem::getInstance()->getFileName(inputFileName)
+			);
+			Console::println("Exporting model: " + outputFileName);
+			TMWriter::write(
+				model,
+				FileSystem::getInstance()->getPathName(outputFileName),
+				FileSystem::getInstance()->getFileName(outputFileName)
+			);
+		} catch (Exception& exception) {
+			Console::println("An error occurred: " + string(exception.what()));
+		}
 	}
 }
