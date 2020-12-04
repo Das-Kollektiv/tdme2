ae64363b14f0fe1559b9f33e8c6a26023904b53f
Andreas Drewke <andreas@drewke.net>
2020-12-04 04:09:32 +0100

    Ooops. Forgot to check in

---
+++ src/tdme/utilities/Reference.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <tdme/utilities/fwd-tdme.h>
+
+/**
+ * Reference counter implementation to be used with inheritance
+ * @author Andreas Drewke
+ */
+class tdme::utilities::Reference {
+public:
+	/**
+	 * @brief Public constructor
+	 */
+	Reference();
+
+	/**
+	 * @brief destructor
+	 */
+	virtual ~Reference();
+
+	/**
+	 * @brief acquires a reference, incrementing the counter
+	 */
+	void acquireReference();
+
+	/**
+	 * @brief releases a reference, thus decrementing the counter and delete it if reference counter is zero
+	 */
+	void releaseReference();
+
+	/**
+	 * Callback method to be overridden, will be called if object will be deleted
+	 */
+	virtual void onDelete();
+
+private:
+	unsigned int referenceCounter;
+};
