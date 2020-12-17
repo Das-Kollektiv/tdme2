51de8e477405a166ba268b89a5f02be6e195786d
Andreas Drewke <andreas@drewke.net>
2020-12-17 22:32:52 +0100

    Update fwd-tdme.h

--- src/tdme/application/fwd-tdme.h
+++ src/tdme/application/fwd-tdme.h
@@ -1,9 +1,9 @@
-// Forward declarations for tdme.engine
+// Forward declarations for tdme.application
 #pragma once
 
 namespace tdme {
 namespace application {
-		class Application;
-		class InputEventHandler;
+	class Application;
+	class InputEventHandler;
 }  // namespace application
 }  // namespace tdme
