8dc63ddc176c4c5f8b3f2e9e517a64f5d74d097a
Andreas Drewke <andreas@drewke.net>
2021-02-05 12:42:25 +0100

    shaders: foliage: small improvement

--- shader/gl2/functions/create_foliage_transform_matrix.inc.glsl
+++ shader/gl2/functions/create_foliage_transform_matrix.inc.glsl
@@ -1,6 +1,6 @@
 mat4 createFoliageTransformMatrix(vec3 origin, vec3 vertex, vec3 translation) {
 	float zAxisRotationBase = mod((float(time * speed * 60.0) + (origin.x + translation.x) * 8.0 * (translation.x + origin.x < 0.0?-1.0:1.0)), 160.0);
-	float zAxisRotation = amplitudeDefault + sin((clamp((((zAxisRotation < 160.0 / 2.0?zAxisRotation:160.0 - zAxisRotation) - (160.0 / 4.0)) * 1.0 / 8.0) * ((vertex.y - origin.y) * 2.0), -20.0, +20.0)) / 20.0 * 3.14) * amplitudeMax;
+	float zAxisRotation = (amplitudeDefault * ((vertex.y - origin.y) * 2.0)) + sin((clamp((((zAxisRotation < 160.0 / 2.0?zAxisRotation:160.0 - zAxisRotation) - (160.0 / 4.0)) * 1.0 / 8.0) * ((vertex.y - origin.y) * 2.0), -20.0, +20.0)) / 20.0 * 3.14) * amplitudeMax;
 	return
 		createTranslationMatrix(vec3(origin.x, origin.y, origin.z)) *
 		createRotationMatrix(vec3(0.0, 0.0, 1.0), zAxisRotation) *
--- shader/gl3/functions/create_foliage_transform_matrix.inc.glsl
+++ shader/gl3/functions/create_foliage_transform_matrix.inc.glsl
@@ -4,7 +4,7 @@ float fmod(float x, float y) {
 
 mat4 createFoliageTransformMatrix(vec3 origin, vec3 vertex, vec3 translation) {
 	float zAxisRotationBase = fmod(((time * speed * 60.0) + ((origin.x + translation.x) * 8.0 * (translation.x + origin.x < 0.0?-1.0:1.0))), 160.0);
-	float zAxisRotation = amplitudeDefault + sin((clamp((((zAxisRotationBase < 160.0 / 2?zAxisRotationBase:160.0 - zAxisRotationBase) - (160.0 / 4.0)) * 1.0 / 8.0) * ((vertex.y - origin.y) * 2.0), -20.0, +20.0)) / 20.0 * 3.14) * amplitudeMax;
+	float zAxisRotation = (amplitudeDefault * ((vertex.y - origin.y) * 2.0)) + sin((clamp((((zAxisRotationBase < 160.0 / 2?zAxisRotationBase:160.0 - zAxisRotationBase) - (160.0 / 4.0)) * 1.0 / 8.0) * ((vertex.y - origin.y) * 2.0), -20.0, +20.0)) / 20.0 * 3.14) * amplitudeMax;
 	return
 		createTranslationMatrix(vec3(origin.x, origin.y, origin.z)) *
 		createRotationMatrix(vec3(0.0, 0.0, 1.0), zAxisRotation) *
