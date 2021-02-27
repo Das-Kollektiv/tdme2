6ab4f7311811061e4ed2e31ebccfefec940ef714
Andreas Drewke <andreas@drewke.net>
2021-02-27 02:20:48 +0100

    tools/terraineditor: some WIP on loading terrain foliage also

--- src/tdme/engine/fileio/prototypes/PrototypeReader.cpp
+++ src/tdme/engine/fileio/prototypes/PrototypeReader.cpp
@@ -27,6 +27,7 @@
 #include <tdme/engine/prototype/PrototypeTerrain.h>
 #include <tdme/engine/EntityShaderParameters.h>
 #include <tdme/engine/LODObject3D.h>
+#include <tdme/engine/Rotation.h>
 #include <tdme/engine/ShaderParameter.h>
 #include <tdme/math/Vector3.h>
 #include <tdme/os/filesystem/FileSystem.h>
@@ -69,6 +70,7 @@ using tdme::engine::prototype::PrototypePhysics_BodyType;
 using tdme::engine::prototype::PrototypeTerrain;
 using tdme::engine::EntityShaderParameters;
 using tdme::engine::LODObject3D;
+using tdme::engine::Rotation;
 using tdme::engine::ShaderParameter;
 using tdme::math::Vector3;
 using tdme::os::filesystem::FileSystem;
@@ -276,7 +278,37 @@ Prototype* PrototypeReader::read(int id, const string& pathName, Value& jEntityR
 			}
 		}
 		{
-
+			auto& jFoliage = jTerrain["f"];
+			for (auto jFoliagePrototypeIt = jFoliage.MemberBegin(); jFoliagePrototypeIt != jFoliage.MemberEnd(); ++jFoliagePrototypeIt) {
+				auto& jFoliagePrototype = jFoliage[jFoliagePrototypeIt->name.GetString()];
+				auto prototypeFile = jFoliagePrototype["f"].GetString();
+				auto jFoliagePrototypePartitions = jFoliagePrototype["t"].GetArray();
+				for (auto foliagePrototypePartitionIdx = 0; foliagePrototypePartitionIdx < jFoliagePrototypePartitions.Size(); foliagePrototypePartitionIdx++) {
+					auto jFoliagePrototypePartitionTransformations = jFoliagePrototypePartitions[foliagePrototypePartitionIdx].GetArray();
+					for (auto jFoliagePrototypePartitionTransformationsIdx = 0; jFoliagePrototypePartitionTransformationsIdx < jFoliagePrototypePartitionTransformations.Size(); jFoliagePrototypePartitionTransformationsIdx++) {
+						Value& jFoliagePrototypeTransformations = jFoliagePrototypePartitionTransformations[jFoliagePrototypePartitionTransformationsIdx];
+						Transformations foliagePrototypeTransformations;
+						foliagePrototypeTransformations.setTranslation(
+							Vector3(
+								jFoliagePrototypeTransformations["tx"].GetFloat(),
+								jFoliagePrototypeTransformations["ty"].GetFloat(),
+								jFoliagePrototypeTransformations["tz"].GetFloat()
+							)
+						);
+						foliagePrototypeTransformations.addRotation(Rotation::Z_AXIS, jFoliagePrototypeTransformations["rz"].GetFloat());
+						foliagePrototypeTransformations.addRotation(Rotation::Y_AXIS, jFoliagePrototypeTransformations["ry"].GetFloat());
+						foliagePrototypeTransformations.addRotation(Rotation::X_AXIS, jFoliagePrototypeTransformations["rx"].GetFloat());
+						foliagePrototypeTransformations.setScale(
+							Vector3(
+								jFoliagePrototypeTransformations["sx"].GetFloat(),
+								jFoliagePrototypeTransformations["sy"].GetFloat(),
+								jFoliagePrototypeTransformations["sz"].GetFloat()
+							)
+						);
+						foliagePrototypeTransformations.update();
+					}
+				}
+			}
 		}
 	}
 
