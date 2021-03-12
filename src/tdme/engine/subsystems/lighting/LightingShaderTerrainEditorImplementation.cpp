b19c01aaabe617f7da52b8bfce7bc384e4de4448
Andreas Drewke <andreas@drewke.net>
2021-03-12 18:07:18 +0100

    tools/terraineditor: ooops, forgot about those files

---
+++ src/tdme/engine/subsystems/lighting/LightingShaderTerrainEditorImplementation.cpp
@@ -0,0 +1,85 @@
+#include <tdme/engine/subsystems/lighting/LightingShaderTerrainEditorImplementation.h>
+
+#include <string>
+
+#include <tdme/engine/fileio/textures/TextureReader.h>
+#include <tdme/engine/subsystems/lighting/LightingShaderTerrainImplementation.h>
+#include <tdme/engine/subsystems/lighting/LightingShaderConstants.h>
+#include <tdme/engine/subsystems/manager/TextureManager.h>
+#include <tdme/engine/subsystems/renderer/Renderer.h>
+#include <tdme/engine/Engine.h>
+#include <tdme/os/filesystem/FileSystem.h>
+#include <tdme/os/filesystem/FileSystemInterface.h>
+#include <tdme/utilities/Console.h>
+
+using std::string;
+using std::to_string;
+
+using tdme::engine::fileio::textures::TextureReader;
+using tdme::engine::subsystems::lighting::LightingShaderBaseImplementation;
+using tdme::engine::subsystems::lighting::LightingShaderConstants;
+using tdme::engine::subsystems::lighting::LightingShaderTerrainEditorImplementation;
+using tdme::engine::subsystems::manager::TextureManager;
+using tdme::engine::subsystems::renderer::Renderer;
+using tdme::engine::Engine;
+using tdme::os::filesystem::FileSystem;
+using tdme::os::filesystem::FileSystemInterface;
+using tdme::utilities::Console;
+
+LightingShaderTerrainEditorImplementation::LightingShaderTerrainEditorImplementation(Renderer* renderer): LightingShaderTerrainImplementation(renderer)
+{
+	additionalDefinitions = "\n#define HAVE_TERRAIN_SHADER_EDITOR";
+}
+
+const string LightingShaderTerrainEditorImplementation::getId() {
+	return "terraineditor";
+}
+
+void LightingShaderTerrainEditorImplementation::initialize()
+{
+	LightingShaderTerrainImplementation::initialize();
+
+	if (initialized == false) return;
+
+	//
+	initialized = false;
+
+	uniformBrushTextureUnit = renderer->getProgramUniformLocation(programId, "brushTextureUnit");
+	if (uniformBrushTextureUnit == -1) return;
+
+	uniformBrushPosition = renderer->getProgramUniformLocation(programId, "brushPosition");
+	if (uniformBrushPosition == -1) return;
+
+	uniformBrushTextureDimension = renderer->getProgramUniformLocation(programId, "brushTextureDimension");
+	if (uniformBrushTextureDimension == -1) return;
+
+	//
+	initialized = true;
+}
+
+void LightingShaderTerrainEditorImplementation::registerShader() {
+	Engine::registerShader(
+		Engine::ShaderType::SHADERTYPE_OBJECT3D,
+		getId(),
+		{
+			{ "brushDimension", ShaderParameter(Vector2(0.0f, 0.0f)) },
+			{ "brushTexture", ShaderParameter(0) },
+			{ "brushPosition", ShaderParameter(Vector2(0.0f, 0.0f)) }
+		}
+	);
+}
+
+void LightingShaderTerrainEditorImplementation::useProgram(Engine* engine, void* context) {
+	LightingShaderTerrainImplementation::useProgram(engine, context);
+
+	//
+	auto currentTextureUnit = renderer->getTextureUnit(context);
+	renderer->setTextureUnit(context, LightingShaderConstants::SPECULAR_TEXTUREUNIT_TERRAIN_BRUSH);
+	renderer->bindTexture(context, engine->getShaderParameter(getId(), "brushTexture").getIntValue());
+	renderer->setTextureUnit(context, currentTextureUnit);
+
+	//
+	renderer->setProgramUniformInteger(context, uniformBrushTextureUnit, LightingShaderConstants::SPECULAR_TEXTUREUNIT_TERRAIN_BRUSH);
+	renderer->setProgramUniformFloatVec2(context, uniformBrushTextureDimension, engine->getShaderParameter(getId(), "brushDimension").getVector2Value().getArray());
+	renderer->setProgramUniformFloatVec2(context, uniformBrushPosition, engine->getShaderParameter(getId(), "brushPosition").getVector2Value().getArray());
+}
---
+++ src/tdme/engine/subsystems/lighting/LightingShaderTerrainEditorImplementation.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+
+#include <tdme/tdme.h>
+#include <tdme/engine/subsystems/lighting/fwd-tdme.h>
+#include <tdme/engine/subsystems/lighting/LightingShaderTerrainImplementation.h>
+#include <tdme/engine/subsystems/renderer/fwd-tdme.h>
+
+using std::string;
+
+using tdme::engine::subsystems::lighting::LightingShaderTerrainEditorImplementation;
+using tdme::engine::subsystems::renderer::Renderer;
+
+/**
+ * Lighting shader implementation
+ * @author Andreas Drewke
+ * @version $Id$
+ */
+class tdme::engine::subsystems::lighting::LightingShaderTerrainEditorImplementation: public LightingShaderTerrainImplementation
+{
+public:
+	/**
+	 * Public constructor
+	 * @param renderer renderer
+	 */
+	LightingShaderTerrainEditorImplementation(Renderer* renderer);
+
+	// overridden methods
+	virtual const string getId() override;
+	virtual void initialize() override;
+	virtual void registerShader() override;
+	virtual void useProgram(Engine* engine, void* context) override;
+
+private:
+	int32_t uniformBrushTextureUnit { -1 };
+	int32_t uniformBrushPosition { -1 };
+	int32_t uniformBrushTextureDimension { -1 };
+
+};
