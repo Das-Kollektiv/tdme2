3e66b04cd0e16f58c2c2d3d8124259dc2793e45c
Andreas Drewke <andreas@drewke.net>
2021-07-28 09:46:55 +0200

    ui-2.0#2: tools/editor: fixed a small UI bug, also added a backtrace() method to RTTI(for debugging purposes, *nix only for now)

--- src/tdme/utilities/RTTI.h
+++ src/tdme/utilities/RTTI.h
@@ -16,5 +16,11 @@ public:
 	 * @brief Demangle c++ symbol names
 	 * @return demangled name
 	 */
-	static const string demangle(const char* name);
+	static const string demangle(const string& name);
+
+	/**
+	 * @brief Returns the current backtrace as string
+	 * @return backtrace as string
+	 */
+	static const string backtrace();
 };
