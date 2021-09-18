41011673786770a593336a00beda1d01a4f91c10
Andreas Drewke <andreas@drewke.net>
2021-09-18 06:34:00 +0200

    oops: forgot about those files, lol

---
+++ src/tdme/tools/editor/controllers/AboutDialogScreenController.cpp
@@ -0,0 +1,80 @@
+#include <tdme/tools/editor/controllers/AboutDialogScreenController.h>
+
+#include <string>
+
+#include <tdme/engine/Version.h>
+#include <tdme/gui/events/GUIActionListener.h>
+#include <tdme/gui/nodes/GUIElementNode.h>
+#include <tdme/gui/nodes/GUIScreenNode.h>
+#include <tdme/gui/nodes/GUITextNode.h>
+#include <tdme/gui/GUI.h>
+#include <tdme/gui/GUIParser.h>
+#include <tdme/utilities/Console.h>
+#include <tdme/utilities/Exception.h>
+#include <tdme/utilities/MutableString.h>
+#include <tdme/utilities/StringTools.h>
+
+using std::string;
+
+using tdme::engine::Version;
+using tdme::gui::events::GUIActionListenerType;
+using tdme::gui::nodes::GUIElementNode;
+using tdme::gui::nodes::GUIScreenNode;
+using tdme::gui::nodes::GUITextNode;
+using tdme::gui::GUIParser;
+using tdme::tools::editor::controllers::AboutDialogScreenController;
+using tdme::utilities::Console;
+using tdme::utilities::Exception;
+using tdme::utilities::MutableString;
+using tdme::utilities::StringTools;
+
+AboutDialogScreenController::AboutDialogScreenController()
+{
+}
+
+AboutDialogScreenController::~AboutDialogScreenController()
+{
+	screenNode = nullptr;
+}
+
+GUIScreenNode* AboutDialogScreenController::getScreenNode()
+{
+	return screenNode;
+}
+
+void AboutDialogScreenController::initialize()
+{
+	try {
+		screenNode = GUIParser::parse("resources/engine/gui", "popup_about.xml");
+		screenNode->setVisible(false);
+		screenNode->addActionListener(this);
+		required_dynamic_cast<GUITextNode*>(screenNode->getNodeById("about_version"))->setText(MutableString(Version::getVersion()));
+	} catch (Exception& exception) {
+		Console::print(string("AboutDialogScreenController::initialize(): An error occurred: "));
+		Console::println(string(exception.what()));
+	}
+}
+
+void AboutDialogScreenController::dispose()
+{
+}
+
+void AboutDialogScreenController::show()
+{
+	screenNode->setVisible(true);
+	screenNode->layout();
+}
+
+void AboutDialogScreenController::close()
+{
+	screenNode->setVisible(false);
+}
+
+void AboutDialogScreenController::onActionPerformed(GUIActionListenerType type, GUIElementNode* node)
+{
+	if (type == GUIActionListenerType::PERFORMED) {
+		if (StringTools::startsWith(node->getId(), "about_caption_close_") == true) { // TODO: a.drewke, check with DH) {
+			close();
+		}
+	}
+}
---
+++ src/tdme/tools/editor/controllers/AboutDialogScreenController.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <string>
+
+#include <tdme/gui/events/fwd-tdme.h>
+#include <tdme/gui/events/GUIActionListener.h>
+#include <tdme/gui/nodes/fwd-tdme.h>
+#include <tdme/tools/editor/controllers/ScreenController.h>
+#include <tdme/utilities/fwd-tdme.h>
+
+using std::string;
+
+using tdme::gui::events::GUIActionListener;
+using tdme::gui::events::GUIActionListenerType;
+using tdme::gui::nodes::GUIScreenNode;
+using tdme::tools::editor::controllers::ScreenController;
+using tdme::utilities::MutableString;
+
+/**
+ * About dialog screen controller
+ * @author Andreas Drewke
+ * @version $Id$
+ */
+class tdme::tools::editor::controllers::AboutDialogScreenController final
+	: public ScreenController
+	, public virtual GUIActionListener
+{
+
+private:
+	GUIScreenNode* screenNode { nullptr };
+
+public:
+	/**
+	 * Public constructor
+	 */
+	AboutDialogScreenController();
+
+	/**
+	 * Destructor
+	 */
+	virtual ~AboutDialogScreenController();
+
+	GUIScreenNode* getScreenNode() override;
+	void initialize() override;
+	void dispose() override;
+	void onActionPerformed(GUIActionListenerType type, GUIElementNode* node) override;
+
+	/**
+	 * Shows the pop up
+	 */
+	void show();
+	/**
+	 * Closes the pop up
+	 */
+	void close();
+
+};
