6f569d7aa6ab546a53617b36fd316ca4df9f3c18
Andreas Drewke <andreas@drewke.net>
2022-01-10 21:33:35 +0100

    ooops, forgot about this file

---
+++ src/tdme/engine/Version.cpp
@@ -0,0 +1,17 @@
+#include <tdme/engine/Version.h>
+
+#include <string>
+
+#include <tdme/tdme.h>
+
+using std::string;
+
+using tdme::engine::Version;
+
+string Version::getVersion() {
+	return "1.1.114";
+}
+
+string Version::getCopyright() {
+	return "Developed 2012-2021 by Andreas Drewke, Dominik Hepp, Mathias Lenz, Kolja Gumpert, drewke.net, mindty.com, github.com/mahula. Please see the license @ https://github.com/andreasdr/tdme2/blob/master/LICENSE";
+}
