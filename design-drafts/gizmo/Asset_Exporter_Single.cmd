61d186dd1207f217543363781a3a8a451679f501
Tepilogic <dominikhepp@mindty.com>
2022-02-04 18:31:08 +0100

    UI: - added missing rotate gizmo

--- design-drafts/gizmo/Asset_Exporter_Single.cmd
+++ design-drafts/gizmo/Asset_Exporter_Single.cmd
@@ -7,6 +7,6 @@ cd D:\Projekte\AScottishWarstory\tdme2
 D:\Projekte\AScottishWarstory\tdme2\bin\tdme\tools\cli\importtmodel  D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_all.tmodel D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_all.glb
 D:\Projekte\AScottishWarstory\tdme2\bin\tdme\tools\cli\importtmodel  D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_translate.tmodel D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_translate.glb
 D:\Projekte\AScottishWarstory\tdme2\bin\tdme\tools\cli\importtmodel  D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_scale.tmodel D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_scale.glb
-D:\Projekte\AScottishWarstory\tdme2\bin\tdme\tools\cli\importtmodel  D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_rotation.tmodel D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_rotation.glb
+D:\Projekte\AScottishWarstory\tdme2\bin\tdme\tools\cli\importtmodel  D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_rotate.tmodel D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_rotate.glb
 D:\Projekte\AScottishWarstory\tdme2\bin\tdme\tools\cli\importtmodel  D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_transscale.tmodel D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo_transscale.glb
 del D:\Projekte\AScottishWarstory\tdme2\resources\engine\models\gizmo*.glb
