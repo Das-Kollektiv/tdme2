0417ac8e0503c7d74d0b4a137dcceb514bf78a8b
Andreas Drewke <andreas@drewke.net>
2022-05-04 11:49:12 +0200

    decals: oops, forgot about those files, uh

---
+++ src/tdme/engine/prototype/PrototypeDecal.cpp
@@ -0,0 +1,45 @@
+#include <tdme/engine/prototype/PrototypeDecal.h>
+
+#include <string>
+
+#include <tdme/tdme.h>
+#include <tdme/engine/fileio/textures/Texture.h>
+#include <tdme/engine/fileio/textures/TextureReader.h>
+#include <tdme/tools/editor/misc/Tools.h>
+
+using std::string;
+
+using tdme::engine::fileio::textures::Texture;
+using tdme::engine::fileio::textures::TextureReader;
+using tdme::tools::editor::misc::Tools;
+
+using tdme::engine::prototype::PrototypeDecal;
+
+PrototypeDecal::PrototypeDecal()
+{
+}
+
+void PrototypeDecal::setTextureFileName(const string& textureFileName, const string& transparencyTextureFileName) {
+	if (texture != nullptr) texture->releaseReference();
+	texture = nullptr;
+	this->textureFileName = textureFileName;
+	this->transparencyTextureFileName = transparencyTextureFileName;
+	if (this->transparencyTextureFileName.empty() == false && this->textureFileName.empty() == false) {
+		texture = TextureReader::read2(
+			Tools::getPathName(this->textureFileName),
+			Tools::getFileName(this->textureFileName),
+			Tools::getPathName(this->transparencyTextureFileName),
+			Tools::getFileName(this->transparencyTextureFileName)
+		);
+	} else
+	if (this->textureFileName.empty() == false) {
+		texture = TextureReader::read(
+			Tools::getPathName(this->textureFileName),
+			Tools::getFileName(this->textureFileName)
+		);
+	}
+}
+
+PrototypeDecal::~PrototypeDecal() {
+	if (texture != nullptr) texture->releaseReference();;
+}
