75a7a6a856d91eb9dace9522c156fed3185f9e0c
Andreas Drewke <andreas@drewke.net>
2022-06-11 22:58:02 +0200

    port-mingw: some more freetype2 dependencies :DDD

--- scripts/windows-mingw-prepare-platform-libs.sh
+++ scripts/windows-mingw-prepare-platform-libs.sh
@@ -9,4 +9,16 @@ cp /mingw64/bin/libopenal-1.dll platform/libs/
 cp /mingw64/bin/libstdc++-6.dll platform/libs/
 cp /mingw64/bin/libwinpthread-1.dll platform/libs/
 cp /mingw64/bin/libfreetype-6.dll platform/libs/
+# libfreetype-6 dependenciens
+cp /mingw64/bin/libbz2-1.dll platform/libs/
+cp /mingw64/bin/libharfbuzz-0.dll platform/libs/
+cp /mingw64/bin/libbrotlidec.dll platform/libs/
+cp /mingw64/bin/libbrotlicommon.dll platform/libs/
+cp /mingw64/bin/libpng16-16.dll platform/libs/
+cp /mingw64/bin/libfreetype-6.dll platform/libs/
+cp /mingw64/bin/libglib-2.0-0.dll platform/libs/
+cp /mingw64/bin/libgraphite2.dll platform/libs/
+cp /mingw64/bin/libiconv-2.dll platform/libs/
+cp /mingw64/bin/libintl-8.dll platform/libs/
+#
 cp /mingw64/bin/addr2line.exe platform/libs/
