af2150feb4ffd6c9a209b01f83485d7ef43246f4
Andreas Drewke <andreas@drewke.net>
2022-10-07 14:39:33 +0200

    scripts: have a windows transpile script batch file

---
+++ scripts/transpile-scripts.bat
@@ -0,0 +1,4 @@
+.\bin\tdme\tools\cli\miniscripttranspiler resources\tests\scripts\advanced-test.tscript src\tdme\tests\MiniScriptAdvancedTest.cpp
+.\bin\tdme\tools\cli\miniscripttranspiler resources\tests\scripts\base-test.tscript src\tdme\tests\MiniScriptBaseTest.cpp
+.\bin\tdme\tools\cli\miniscripttranspiler resources\tests\scripts\emit-test.tscript src\tdme\tests\MiniScriptEmitTest.cpp
+.\bin\tdme\tools\cli\miniscripttranspiler resources\tests\scripts\functions-test.tscript src\tdme\tests\MiniScriptFunctionsTest.cpp
