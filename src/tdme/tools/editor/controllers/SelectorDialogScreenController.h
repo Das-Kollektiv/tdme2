8a7fd270360ee1e07445a17c584f92c773e70468
Andreas Drewke <andreas@drewke.net>
2023-04-27 19:01:10 +0200

    ooops, forgot about those files

---
+++ src/tdme/tools/editor/controllers/SelectorDialogScreenController.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <string>
+
+#include <tdme/tdme.h>
+#include <tdme/gui/events/fwd-tdme.h>
+#include <tdme/gui/events/GUIActionListener.h>
+#include <tdme/gui/events/GUIFocusListener.h>
+#include <tdme/gui/events/GUITooltipRequestListener.h>
+#include <tdme/gui/nodes/fwd-tdme.h>
+#include <tdme/tools/editor/controllers/ScreenController.h>
+#include <tdme/tools/editor/misc/fwd-tdme.h>
+#include <tdme/utilities/fwd-tdme.h>
+
+using std::string;
+
+using tdme::gui::events::GUIActionListener;
+using tdme::gui::events::GUIActionListenerType;
+using tdme::gui::events::GUIFocusListener;
+using tdme::gui::events::GUITooltipRequestListener;
+using tdme::gui::nodes::GUINode;
+using tdme::gui::nodes::GUIScreenNode;
+using tdme::tools::editor::controllers::ScreenController;
+using tdme::tools::editor::misc::PopUps;
+using tdme::utilities::MutableString;
+
+/**
+ * Selector dialog screen controller
+ * @author Andreas Drewke
+ */
+class tdme::tools::editor::controllers::SelectorDialogScreenController final
+	: public ScreenController
+	, public virtual GUIActionListener
+	, public virtual GUIFocusListener
+	, public virtual GUITooltipRequestListener
+{
+
+private:
+	PopUps* popUps { nullptr };
+	GUIScreenNode* screenNode { nullptr };
+
+public:
+	/**
+	 * Public constructor
+	 * @param popUps pop ups
+	 */
+	SelectorDialogScreenController(PopUps* popUps);
+
+	/**
+	 * Destructor
+	 */
+	virtual ~SelectorDialogScreenController();
+
+	// overridden methods
+	GUIScreenNode* getScreenNode() override;
+	void initialize() override;
+	void dispose() override;
+	void onAction(GUIActionListenerType type, GUIElementNode* node) override;
+	void onFocus(GUIElementNode* node) override;
+	void onUnfocus(GUIElementNode* node) override;
+	void onTooltipShowRequest(GUINode* node, int mouseX, int mouseY) override;
+	void onTooltipCloseRequest() override;
+
+	/**
+	 * Shows the pop up
+	 */
+	void show();
+	/**
+	 * Closes the pop up
+	 */
+	void close();
+
+};
