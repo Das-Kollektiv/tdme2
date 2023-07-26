c9c83e0a3351303bd6d51732f3215c4b08dc3c49
Andreas Drewke <andreas@drewke.net>
2023-07-26 23:30:38 +0200

    oh, i spotted a nice new c++ feature i like to use, see map test

--- src/tdme/tests/ContainerTest-main.cpp
+++ src/tdme/tests/ContainerTest-main.cpp
@@ -1,3 +1,4 @@
+#include <map>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -6,6 +7,7 @@
 #include <tdme/utilities/Console.h>
 #include <tdme/utilities/StringTools.h>
 
+using std::map;
 using std::string;
 using std::to_string;
 using std::vector;
@@ -15,19 +17,34 @@ using tdme::utilities::StringTools;
 
 int main(int argc, char** argv)
 {
-	vector<int> v = { 1, 2, 3, 4, 5 };
-	Console::println("v = { 1, 2, 3, 4, 5 }, capacity = " + to_string(v.capacity()));
-	v.clear();
-	Console::println("v.clear(), capacity = " + to_string(v.capacity()));
-	v = { 1, 2, 3 };
-	Console::println("v = { 1, 2, 3 }, capacity = " + to_string(v.capacity()));
-	v.resize(2);
-	Console::println("v.resize(2), capacity = " + to_string(v.capacity()));
-	v.resize(6);
-	Console::println("v.resize(6), capacity = " + to_string(v.capacity()));
-	v.resize(12);
-	Console::println("v.resize(12), capacity = " + to_string(v.capacity()));
-	v.resize(3);
-	Console::println("v.resize(3), capacity = " + to_string(v.capacity()));
+	// vector tests
+	{
+		vector<int> vector = { 1, 2, 3, 4, 5 };
+		Console::println("vector = { 1, 2, 3, 4, 5 }, capacity = " + to_string(vector.capacity()));
+		vector.clear();
+		Console::println("vector.clear(), capacity = " + to_string(vector.capacity()));
+		vector = { 1, 2, 3 };
+		Console::println("vector = { 1, 2, 3 }, capacity = " + to_string(vector.capacity()));
+		vector.resize(2);
+		Console::println("vector.resize(2), capacity = " + to_string(vector.capacity()));
+		vector.resize(6);
+		Console::println("vector.resize(6), capacity = " + to_string(vector.capacity()));
+		vector.resize(12);
+		Console::println("vector.resize(12), capacity = " + to_string(vector.capacity()));
+		vector.resize(3);
+		Console::println("vector.resize(3), capacity = " + to_string(vector.capacity()));
+	}
+	// map tests
+	{
+		map<string, string> map {
+			{"0", "a"},
+			{"1", "b"},
+			{"2", "c"}
+		};
+		for (auto& [key, value]: map) {
+			Console::println(key + " -> " + value);
+		}
+	}
+	//
     return 0;
 }
