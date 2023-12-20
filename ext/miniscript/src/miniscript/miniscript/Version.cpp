d1e2cf424dae7b8e528bba15a90122283a7406c2
Andreas Drewke <andreas@drewke.net>
2023-12-20 19:20:18 +0100

    ext/miniscript: bumped version

--- ext/miniscript/src/miniscript/miniscript/Version.cpp
+++ ext/miniscript/src/miniscript/miniscript/Version.cpp
@@ -9,7 +9,7 @@ using std::string;
 using miniscript::miniscript::Version;
 
 string Version::getVersion() {
-	return "0.9.12 PRE-BETA";
+	return "0.9.14 PRE-BETA";
 }
 
 string Version::getCopyright() {
