2395b6cefcdaa9a46d9f3c177d738b57e61591de
Andreas Drewke <andreas@drewke.net>
2025-01-02 22:38:57 +0100

    updatelibs: commit / 4

---
+++ resources/minitscript/templates/transpilation/Transpilation.cpp
@@ -0,0 +1,26 @@
+#include "{$class-name}.h"
+
+#include <memory>
+#include <stack>
+
+#include <minitscript/minitscript.h>
+#include <minitscript/minitscript/MinitScript.h>
+
+using std::make_unique;
+using std::stack;
+using std::unique_ptr;
+
+#include <{$base-class-header}>
+
+/*__MINITSCRIPT_TRANSPILEDMINITSCRIPTCODE_INCLUDES_START__*/
+/*__MINITSCRIPT_TRANSPILEDMINITSCRIPTCODE_INCLUDES_END__*/
+
+/*__MINITSCRIPT_TRANSPILEDMINITSCRIPTCODE_USINGS_START__*/
+/*__MINITSCRIPT_TRANSPILEDMINITSCRIPTCODE_USINGS_END__*/
+
+{$class-name}::{$class-name}(): {$base-class}() {
+	initializeNative();
+}
+
+/*__MINITSCRIPT_TRANSPILEDMINITSCRIPTCODE_DEFINITIONS_START__*/
+/*__MINITSCRIPT_TRANSPILEDMINITSCRIPTCODE_DEFINITIONS_END__*/
