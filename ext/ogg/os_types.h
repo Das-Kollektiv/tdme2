2f83947c67086ddb5629a2d4892b5284da648954
Andreas Drewke <andreas@drewke.net>
2025-01-08 20:57:54 +0100

    NetBSD: this should fix the build

--- ext/ogg/os_types.h
+++ ext/ogg/os_types.h
@@ -159,6 +159,15 @@
    typedef uint32_t ogg_uint32_t;
    typedef int64_t ogg_int64_t;
 
+#elif defined(__NetBSD__)
+#  include <inttypes.h>
+
+   typedef int16_t ogg_int16_t;
+   typedef uint16_t ogg_uint16_t;
+   typedef int32_t ogg_int32_t;
+   typedef uint32_t ogg_uint32_t;
+   typedef int64_t ogg_int64_t;
+
 #else
 
 #  include <ogg/config_types.h>
